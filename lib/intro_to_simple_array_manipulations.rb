112f4f9546901980134b93818a33191c1f872e65
Jesse Vaughn <jcv221@gmail.com>
2020-09-13 02:45:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,3 +3,7 @@ def using_concat(array1, array2)
   array2 = ["sports cars", "flatiron school"]
   array1.concat(["sports cars", "flatiron school"])
 end
+
+def using_insert(array, element)
+  array.insert(element)
+end
